d8627667afdc9ff2c18b02995d0ec27b40f20def
natalia <ailatangh@gmail.com>
2017-06-19 12:08:38 +0200

    new html and js modal

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var modal = document.getElementById('modal-container');
+var cancelButton = document.getElementById('cancel-button');
+var acceptButon = document.getElementById('accept-button');
+var buttonToErase = document.getElementById('trash');
+
+function showModal (){
+  modal.style.display = 'inline';
+}
+function occultModal(){
+  modal.style.display = 'none';
+}
+
+function acceptErase(){
+  // function to connect with server;
+  occultModal();
+}
+
+cancelButton.addEventListener('click', occultModal);
+buttonToErase.addEventListener('click', showModal);
+acceptButon.addEventListener('click', acceptErase);
