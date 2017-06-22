363897ebfc294d3e2c328be20cdc81d340434d18
natalia <ailatangh@gmail.com>
2017-06-22 16:19:27 +0200

    added Urls for the buttons

--- js/product.js
+++ js/product.js
@@ -32,33 +32,3 @@ function toggleViewProduct() {
 
 labelProduct.addEventListener("click", toggleViewProduct);
 labelMap.addEventListener("click", toggleViewProduct);
-
-// GESTIÓN
-//Input
-var selectUser = document.getElementById("selectUser");
-var selectRoles = document.getElementById("selectRol");
-// Label
-var labelUser = document.getElementById("loadUser");
-var labelRoles = document.getElementById("loadRoles");
-// Div product and Map
-var rolesElement = document.querySelector(".listRoles");
-var userElement = document.querySelector(".listUser");
-// Button
-var btnUser = document.querySelector(".btn-user");
-
-function toggleViewGestion() {
-  //Si la opcion con id selectProduct (dentro del documento) >
-  if (selectUser.checked == true) {
-    //esta activada pone propiedad "hidden" y desactiva la del otro elemento
-    userElement.classList.toggle("hidden");
-    rolesElement.classList.toggle("hidden");
-    titlePages.innerHTML = "Usuarios";
-  } else {
-    userElement.classList.toggle("hidden");
-    rolesElement.classList.toggle("hidden");
-    titlePages.innerHTML = "Roles";
-  }
-}
-
-labelUser.addEventListener("click", toggleViewGestion);
-labelRoles.addEventListener("click", toggleViewGestion);
