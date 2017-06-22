6d8433c888ffa9a0bf6c93d3297fb2e619fdfd92
Elena Mateos Lopez <elena.mateos1@gmail.com>
2017-06-22 18:10:35 +0200

    change some clases at html for conflicts with clases at gestion page, cause there is 2 pages at once

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var modal = document.getElementById('modal-container');
+var modal = document.querySelector('#modal-container');
 var cancelButton = document.getElementById('cancel-button');
 var acceptButon = document.getElementById('accept-button');
 var buttonToErase = document.getElementById('trash');
@@ -20,3 +20,37 @@ function acceptErase(){
 cancelButton.addEventListener('click', occultModal);
 buttonToErase.addEventListener('click', showModal);
 acceptButon.addEventListener('click', acceptErase);
+
+//------------------
+
+// var buttonToErase = document.getElementById('trash');
+//
+// var cancelButton = document.getElementById('cancel-button');
+// var acceptButon = document.getElementById('accept-button');
+//
+//
+// function showOrHideModal(){
+//   var modal= document.getElementById('modal-container');
+//   modal.classList.toggle('showModal');
+// }
+//
+//
+// function acceptErase(){
+//   // function to connect with server;
+//   showOrHideModal();
+// }
+//
+// cancelButton.addEventListener('click', showOrHideModal);
+// buttonToErase.addEventListener('click', showOrHideModal);
+// acceptButon.addEventListener('click', acceptErase);
+//
+//
+// // var button = document.querySelector('#botonMenu');
+// //
+// // function showOrHideMobileMenu(){
+// //
+// //   var menu = document.querySelector('#menu');
+// //   menu.classList.toggle('show-menu');
+// // }
+// //
+// // button.addEventListener('click', showOrHideMobileMenu);
