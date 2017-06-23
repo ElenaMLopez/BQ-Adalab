2f766079869adb56e45375823f465c026348d8e9
natalia <ailatangh@gmail.com>
2017-06-23 16:17:50 +0200

    modified function modal and html

--- js/modal.js
+++ js/modal.js
@@ -4,15 +4,25 @@ var modalUser = document.getElementById('modal-user');
 var modalRol = document.getElementById('modal-rol');
 var cancelButton = document.getElementById('cancel-button');
 var acceptButon = document.getElementById('accept-button');
-var buttonToErase = document.querySelectorAll('.trash');
+var trashUser = document.querySelectorAll('.trashUser');
+var trashRol = document.querySelectorAll('.trashRol');
 
-function showModal (){
+// Function Modal User
+function showModalUser (event){
   modalUser.classList.toggle('hidden');
+}
+
+for (var i = 0; i < buttonToErase.length; i++) {
+  buttonToErase[i].addEventListener('click', showModal, false);
+}
+
+// Function Modal Roles
+function showModalRol (event){
   modalRol.classList.toggle('hidden');
-  for (var i = 0; i < buttonToErase.length; i++) {
-    buttonToErase[i].addEventListener('click', showModal)
-  }
-  console.log(buttonToErase);
+}
+
+for (var i = 0; i < buttonToErase.length; i++) {
+  buttonToErase[i].addEventListener('click', showModalRol, false);
 }
 
 function occultModal(){
